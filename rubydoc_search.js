cf40c21e02995b5a7edd36bd749cfcc6cc918c71
Mr S Doig <sean.doig@gmail.com>
2013-05-24 22:40:23 +0100

    Fix indentation.  Again.

--- rubydoc_search.js
+++ rubydoc_search.js
@@ -3,91 +3,99 @@ var API_KEY = "AIzaSyCPQGZImN1P6OZ_szYe7oWpSQgG0Rie9Ns";
 var currentQueryString;
 var resultCache = {};
 
-chrome.omnibox.onInputChanged.addListener(_.debounce(function (queryText, suggestCallback) {
-    console.log("Changed:", queryText);
-	
-	// Prepend preferred version to query if it exists
-	preferredVersion = localStorage.getItem("rdoc_preferred_version");
-	if (preferredVersion){
-		queryText = preferredVersion + " " + queryText;
-	}
-		
-    currentQueryString = queryText;
+chrome.omnibox.onInputChanged.addListener(_.debounce(function (queryText,
+  suggestCallback) {
+  console.log("Changed:", queryText);
 
-    if (!queryText) return;
+  // Prepend preferred version to query if it exists
+  preferredVersion = localStorage.getItem("rdoc_preferred_version");
+  if (preferredVersion) {
+    queryText = preferredVersion + " " + queryText;
+  }
 
-    function dataHandler (data) {
-        if (currentQueryString !== queryText) {
-            // We went past this query, but cache it anyway
-            resultCache[queryText] = data;
-            return;
-        }
+  currentQueryString = queryText;
 
-        var itemCountToConsider;
+  if (!queryText) return;
 
-        _(data.items).
-            chain().
-            tap(function (arr) { itemCountToConsider = arr.length < 5 ? arr.length : 5 }).
-            first(itemCountToConsider).
-            map(function (item) {
-				      var title = item.htmlTitle.replace(/<b>/gi, "").replace(/<\/b>/gi, "");
-				      title = title.replace(/(.*)\((Ruby [0-9\.]*)\)[ ]*- Ruby-Doc/gi, "$2 - <match>$1</match>");
-				      var description = "<dim>" + title + "</dim> - <url>" + item.htmlFormattedUrl + "</url>";
-	            return {
-	                content    : item.link,
-	                description: description
-	            };
-            }).
-            tap(suggestCallback);
+  function dataHandler(data) {
+    if (currentQueryString !== queryText) {
+      // We went past this query, but cache it anyway
+      resultCache[queryText] = data;
+      return;
     }
 
-    // Check if we cached results for this query
-    if (resultCache[queryText]) {
-        dataHandler(resultCache[queryText]);
-        return;
-    }
-	
-	getResults(queryText);
-	
-	function getResults(queryText) {
-	    $.getJSON("https://www.googleapis.com/customsearch/v1?callback=?",
-	        {
-	            key   : API_KEY,
-	            alt   : "json",
-	            q     : queryText,
-	            num   : 5,
-	            lr    : "lang_en",
-	            cx    : "008550084672749702097:2xvl9fhluys",
-	            fields: "items(formattedUrl,htmlFormattedUrl,htmlTitle,link,title)"
-	        },
-	        dataHandler);
-	}
-   
+    var itemCountToConsider;
+
+    _(data.items).
+    chain().
+    tap(function (arr) {
+      itemCountToConsider = arr.length < 5 ? arr.length : 5
+    }).
+    first(itemCountToConsider).
+    map(function (item) {
+      var title = item.htmlTitle.replace(/<b>/gi, "").replace(/<\/b>/gi, "");
+      title = title.replace(/(.*)\((Ruby [0-9\.]*)\)[ ]*- Ruby-Doc/gi,
+        "$2 - <match>$1</match>");
+      var description = "<dim>" + title + "</dim> - <url>" + item.htmlFormattedUrl +
+        "</url>";
+      return {
+        content: item.link,
+        description: description
+      };
+    }).
+    tap(suggestCallback);
+  }
+
+  // Check if we cached results for this query
+  if (resultCache[queryText]) {
+    dataHandler(resultCache[queryText]);
+    return;
+  }
+
+  getResults(queryText);
+
+  function getResults(queryText) {
+    $.getJSON("https://www.googleapis.com/customsearch/v1?callback=?", {
+      key: API_KEY,
+      alt: "json",
+      q: queryText,
+      num: 5,
+      lr: "lang_en",
+      cx: "008550084672749702097:2xvl9fhluys",
+      fields: "items(formattedUrl,htmlFormattedUrl,htmlTitle,link,title)"
+    },
+      dataHandler);
+  }
+
 }, 250));
 
 
 
 chrome.omnibox.onInputEntered.addListener(function (queryText) {
-    // Navigate user to selected page or the search page
-    console.log("Entered:", queryText);
+  // Navigate user to selected page or the search page
+  console.log("Entered:", queryText);
 
-    var url;
+  var url;
 
-    if (queryText.indexOf("http://") === -1 && queryText.indexOf("https://") === -1) {
-        url = "http://ruby-doc.com/search.html?cx=011815814100681837392%3Awnccv6st5qk&q="
-                + encodeURIComponent(queryText);
-    } else {
-        url = queryText;
-    }
+  if (queryText.indexOf("http://") === -1 && queryText.indexOf("https://") === -
+    1) {
+    url =
+      "http://ruby-doc.com/search.html?cx=011815814100681837392%3Awnccv6st5qk&q=" +
+      encodeURIComponent(queryText);
+  } else {
+    url = queryText;
+  }
 
-    chrome.tabs.update({url: url});
+  chrome.tabs.update({
+    url: url
+  });
 });
 
 chrome.omnibox.onInputStarted.addListener(function () {
-    console.log("Started");
+  console.log("Started");
 });
 
 chrome.omnibox.onInputCancelled.addListener(function () {
-    currentQueryString = "";
-    console.log("Cancelled");
+  currentQueryString = "";
+  console.log("Cancelled");
 });
