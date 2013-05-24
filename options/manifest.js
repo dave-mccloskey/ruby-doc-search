a01eb54a634c8b4b80ec6aa5056a5ba45c0a32e0
Mr S Doig <sean.doig@gmail.com>
2013-05-24 21:39:18 +0100

    Improve wording of options page.

--- options/manifest.js
+++ options/manifest.js
@@ -4,16 +4,16 @@ this.manifest = {
     "icon": "../rubydoc-48.png",
     "settings": [
         {
-            "tab": "Preferred Version",
+            "tab": "Preferred Ruby Version",
             "group": "Set preferred version",
 			"name": "rdoc_preferred_version",
 			"id": "rdoc_preferred_version",
             "type": "text",
-            "label": "Preferred Version",
+            "label": "Ruby Version",
             "text": "The full version number e.g 2.0, 1.9.3..."
         },
 		{
-            "tab": "Preferred Version",
+            "tab": "Preferred Ruby Version",
             "group": "Set preferred version",
             "name": "submit_version",
             "type": "button",
@@ -22,11 +22,11 @@ this.manifest = {
         },
 		
 		{
-			"tab": "Preferred Version",
+			"tab": "Preferred Ruby Version",
 			"group": "Note",
 			"name": "note",
 			"type": "description",
-			"text": "Setting this preference does not limit results to only that Ruby version, but is pretty good at prioritizing the results.<br><br>It actually works by prepending the contents of the box onto the search, so you may be able to find inventive uses for it."
+			"text": "Setting this preference does not limit results to only that Ruby version, but is pretty good at prioritizing them.<br><br>It actually works by prepending the contents of the box onto the search, so you may be able to find inventive uses for it."
 		}
 		]
 	};
